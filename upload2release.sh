66c3b2e9bbd6d8070b961197b9d55acadef4de71
WorkTimer <worktimer@msn.com>
2019-12-20 15:23:31 +0800

    Update upload2release.sh

--- upload2release.sh
+++ upload2release.sh
@@ -23,9 +23,9 @@ cat $GITHUB_EVENT_PATH
 #RELEASE_ID=$(jq --raw-output '.release.id' $GITHUB_EVENT_PATH)
 #echo RELEASE_ID = $RELEASE_ID
 
-curl --fail -H "Authorization: token ${GITHUB_TOKEN}" "https://api.github.com/repos/${GITHUB_REPOSITORY}/releases"
+curl -s --fail -H "Authorization: token ${GITHUB_TOKEN}" "https://api.github.com/repos/${GITHUB_REPOSITORY}/releases"
 
-RELEASE_ID=$(curl --fail \
+RELEASE_ID=$(curl -s --fail \
     -H "Authorization: token ${GITHUB_TOKEN}" \
     "https://api.github.com/repos/${GITHUB_REPOSITORY}/releases" |
     jq --raw-output ".[] | select(.tag_name == \"$TAG\") | .id")
